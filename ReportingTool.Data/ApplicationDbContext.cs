44ad94a159fdb89ffe5467cee5e4028acd8e25f5
grozdeto <gros2@abv.bg>
2021-01-01 22:10:59 +0200

    Scaffolded database using Database-first, created ReportingTool.Web  (MVC project),  ReportingTool.Services and ReportingTool.Data projects.

---
+++ ReportingTool.Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ReportingTool.Data
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Arrival> Arrivals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=DefaultConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Arrival>(entity =>
+            {
+                entity.ToTable("Arrival");
+
+                entity.Property(e => e.When).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ReportingTool.Data/Arrival.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+#nullable disable
+
+namespace ReportingTool.Data
+{
+    public partial class Arrival
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime When { get; set; }
+    }
+}
